ed5c1388fa10a4a090f4054f20eed376dfa49e5c
吉川 <yoshikawa.hazuki@alhinc.jp>
2026-04-02 11:15:54 +0900

    タスク追加機能修正

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 tasks = []
 
-task = input("タスクを入力してください: ")
-tasks.append(task)
+while True:
+    print("\n1:追加 2:一覧 3:終了")
+    choice = input("選択してください： ")
 
-print(tasks)
+    if choice == "1":
+        task = input("タスクを入力してください： ")
+        tasks.append(task)
+    
+    elif choice == "2":
+        print("=== タスク一覧 ===")
+        for i , t in enumerate(tasks):
+            print(f"{i}: {t}")
+    
+    elif choice == "3":
+        print("終了します")
+        break
+    
+    else:
+        print("無効な入力です")
